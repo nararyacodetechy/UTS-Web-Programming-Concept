71d8e6128c3d875391b2bb9749fbbccd01678ed2
nararyacodetechy <bagusnararyanandaraditya@gmail.com>
2022-04-12 20:16:51 +0800

    Menambahkan Script klik Random Color

--- UTS/script.js
+++ UTS/script.js
@@ -1,6 +1,16 @@
-//Box => Cursor
+//Elemen => Cursor
+const Elemen = document.getElementById("Elemen");
 
-const box = document.querySelector(".box");
 document.addEventListener("mousemove", e => {
-    box.setAttribute("style", "top: " + e.pageY + "px; left: " + e.pageX + "px;")
+    Elemen.setAttribute("style", "top: " + e.pageY + "px; left: " + e.pageX + "px;")
 });
+
+// Random Color Click Elemen
+function random(cal) {
+    return Math.floor(Math.random() * cal);
+}
+
+document.onclick  = function () { 
+randomColor = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+Elemen.style.background = randomColor;
+}
